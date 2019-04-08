29fe4eaf5f9f868aa0ae7f9933efbd2045083f9b
Otha D Hernandez <otha.hernandez@gmail.com>
2019-04-08 02:26:07 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -4,7 +4,7 @@ def valid_move?(board, index)
   if index.between?(0, 8)
     if position_taken?(board, index) == true
       return false
-      elsif index > 8
+      elsif index > 8 
       return false
     end
     
